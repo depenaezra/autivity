4aaf76c9fb7a350500855081c12d0c7fb856ac0a
aquiknowshe <shekainahsandy.aquinoo@gmail.com>
2026-07-16 23:55:35 +0800

    Restructure repository into mobile and web applications

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // This forces the app to immediately skip the old test screen 
-  // and jump straight to your Teacher Homescreen!
-  return <Redirect href="/(auth)" />;
-}
